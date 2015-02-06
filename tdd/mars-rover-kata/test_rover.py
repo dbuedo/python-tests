d07f6abeaebfde701ee8132ea0b3eeea02cc13b8
dbuedo <davidbuedo@gmail.com>
2015-02-06 19:17:04 +0100

    initial step of the Mars rover kata to improve TDD and python skills

---
+++ tdd/mars-rover-kata/test_rover.py
@@ -0,0 +1,13 @@
+
+import rover
+import unittest
+
+class TestRover(unittest.TestCase):
+    def testConstructor(self):
+        r = rover.Rover(10, 12, 'N')
+        self.assertEqual(10, r.x)
+        self.assertEqual(12, r.y)
+        self.assertEqual('N', r.orientation)
+    
+if __name__ == '__main__':
+    unittest.main()
