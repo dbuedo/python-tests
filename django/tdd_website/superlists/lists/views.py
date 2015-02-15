33323e42456acb207b9c384c47ec8a2a074b891c
dbuedo <davidbuedo@gmail.com>
2015-02-15 17:37:45 +0100

    current status

--- django/tdd_website/superlists/lists/views.py
+++ django/tdd_website/superlists/lists/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    response = HttpResponse()
-    response.content = '<html><title>To-Do lists</title></html>'
-    return response
+	return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+	})
